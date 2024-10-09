2f4d73faad675487584d2791d9caa94ea55c3bea
bevthedev <bevthedev@gmail.com>
2024-10-09 20:33:54 +1100

    Working timesheet without itegration

--- components/wrapper/timesheet-table.tsx
+++ components/wrapper/timesheet-table.tsx
@@ -11,23 +11,23 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { date, map, z } from "zod"
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from "@/components/ui/form"
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 
 
 import DummyTable from "../dummy/table";
 
-const currentWeek : Week[] = getDatesOfCurrentWeek();
-let tableProps : TableProps = {
+const currentWeek: Week[] = getDatesOfCurrentWeek();
+let tableProps: TableProps = {
   title: "",
   columns: currentWeek.map((week) => ({ title: week.day })),
-  rows: currentWeek.map((week) => [<Input key={week.date.getTime()}/>]),
+  rows: currentWeek.map((week) => [<Input key={week.date.getTime()} />]),
 };
 
 
@@ -35,53 +35,63 @@ let tableProps : TableProps = {
 const TimesheetTable = () => {
 
 
-    // Define the base schema for common validation rules
-const baseSchema = z.number().min(2, {
+  // Define the base schema for common validation rules
+  const baseSchema = z.number().min(2, {
     message: "Value must be at least 2.",
   }).max(24, {
     message: "Value must be at most 24.",
   });
-const formSchema = z.object(
-  DaysOfWeek.reduce((schema, day) => {
-    schema[day] = baseSchema;
-    return schema;
-  }, {} as Record<typeof DaysOfWeek[number], typeof baseSchema>)
-);
+  const formSchema = z.object(
+    DaysOfWeek.reduce((schema, day) => {
+      schema[day] = baseSchema;
+      return schema;
+    }, {} as Record<typeof DaysOfWeek[number], typeof baseSchema>)
+  );
 
-        // 1. Define your form.
-        const form = useForm<z.infer<typeof formSchema>>({
-            resolver: zodResolver(formSchema),
-            defaultValues: DaysOfWeek.reduce((values, day) => {
-                values[day] = 0;
-                return values;
-            }, {} as Record<typeof DaysOfWeek[number], number>)
-        });
-         
-          // 2. Define a submit handler.
-          function onSubmit(values: z.infer<typeof formSchema>) {
-            // Do something with the form values.
-            // ✅ This will be type-safe and validated.
-            console.log(values)
-          }
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: DaysOfWeek.reduce((values, day) => {
+      values[day] = 0;
+      return values;
+    }, {} as Record<typeof DaysOfWeek[number], number>)
+  });
 
-          tableProps.rows = currentWeek.map((week) => [
-            <FormField
-            key={week.date.getTime()}
-            control={form.control}
-            name={week.day}   
-            render={({ field }) => (
-                <FormControl>
-                <Input type="number"  {...field}/>
-                </FormControl>
-            )}
-          />
-        ]);
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  tableProps.rows = currentWeek.map((week) => [
+
+    <FormField
+      control={form.control}
+      name={week.day}
+      key={week.date.getTime()}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel></FormLabel>
+          <FormControl>
+            <Input type="number"  {...field}
+              onChange={(e) => field.onChange(Number(e.target.value))} />
+          </FormControl>
+          <FormDescription>
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    >
+    </FormField>
+
+  ]);
 
   return (
 
-<Form {...form}>
+    <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <DummyTable {...tableProps} />
+        <DummyTable {...tableProps} />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
