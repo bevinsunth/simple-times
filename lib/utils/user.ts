142148b5a0f380e957c58c169b3730619c8ebc9d
bevthedev <bevthedev@gmail.com>
2024-11-30 19:20:17 +1100

    Minor updates

--- lib/utils/user.ts
+++ lib/utils/user.ts
@@ -1,13 +1,11 @@
 import { User } from '@prisma/client';
 import { getCurrentUser } from '../session';
 import { getUserByEmail } from './query';
-import { warn } from 'console';
 
 //get logged in user
 export const getLoggedInUser = async (): Promise<User | null> => {
   const user = await getCurrentUser();
   if (!user) {
-    warn('User not logged in');
     return null;
   }
   return await getUserByEmail(user.email as string);
